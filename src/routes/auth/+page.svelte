ea486278781df9aee095b2d2d47b069ea4fdd921
Volkoy <cyred3@gmail.com>
2025-08-17 19:48:45 +0100

    Update sign-in prompt

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -14,7 +14,7 @@
 
 <div class="flex min-h-screen items-center justify-center">
 	<div class="flex w-full max-w-sm flex-col items-center rounded-xl bg-teal-600 px-3 py-7">
-		<h2 class="mb-3 text-2xl font-bold text-white">Sign in</h2>
+		<h2 class="mb-3 text-2xl font-bold text-white">Sign in to get started!</h2>
 		<Button
 			class="flex items-center justify-center gap-2 rounded-lg bg-white px-7 py-5 font-semibold text-teal-700 transition hover:cursor-pointer hover:bg-teal-50"
 			onclick={loginWithGoogle}
